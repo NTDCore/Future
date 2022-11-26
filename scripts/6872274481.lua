469f8a9397ae3ce6d294f2ccc1cb96f437561dbf
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-11-26 18:25:11 +0700

    Update 6872274481.lua

--- scripts/6872274481.lua
+++ scripts/6872274481.lua
@@ -1300,6 +1300,56 @@ do
     })
 end
 
+local funnyFly = {["Enabled"] = false}
+local funnyAura = {["Enabled"] = false}
+
+runcode(function()
+    local part
+    funnyFly = GuiLibrary.Objects.CombatWindow.API.CreateOptionsButton({
+        Name = "FunnyFly",
+        Function = function(callback)
+            if callback then
+                if funnyAura["Enabled"] then funnyAura.ToggleButton(false) end
+                local origy = entity.character.HumanoidRootPart.Position.y
+                part = Instance.new("Part", workspace)
+                part.Size = Vector3.new(1,1,1)
+                part.Transparency = 1
+                part.Anchored = true
+                part.CanCollide = false
+                cam.CameraSubject = part
+                RunLoops:BindToHeartbeat("FunnyFlyPart", 1, function()
+                    local pos = entity.character.HumanoidRootPart.Position
+                    part.Position = Vector3.new(pos.x, origy, pos.z)
+                end)
+                local cf = entity.character.HumanoidRootPart.CFrame
+                entity.character.HumanoidRootPart.CFrame = CFrame.new(cf.x, 300000, cf.z)
+                if entity.character.HumanoidRootPart.Position.X < 50000 then 
+                    entity.character.HumanoidRootPart.CFrame *= CFrame.new(0, 100000, 0)
+                end
+            else
+                RunLoops:UnbindFromHeartbeat("FunnyFlyPart")
+                local pos = entity.character.HumanoidRootPart.Position
+                local rcparams = RaycastParams.new()
+                rcparams.FilterType = Enum.RaycastFilterType.Whitelist
+                rcparams.FilterDescendantsInstances = {workspace.Map}
+                rc = workspace:Raycast(Vector3.new(pos.x, 300, pos.z), Vector3.new(0,-1000,0), rcparams)
+                if rc and rc.Position then
+                    entity.character.HumanoidRootPart.CFrame = CFrame.new(rc.Position) * CFrame.new(0,3,0)
+                end
+                cam.CameraSubject = lplr.Character
+                part:Destroy()
+                RunLoops:BindToHeartbeat("FunnyFlyVeloEnd", 1, function()
+                    entity.character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+                    entity.character.HumanoidRootPart.CFrame = CFrame.new(rc.Position) * CFrame.new(0,3,0)
+                end)
+                task.wait(1)
+                RunLoops:UnbindFromHeartbeat("FunnyFlyVeloEnd")
+                
+            end
+        end
+    })
+end)
+
 do 
     local old2 = bedwars.SharedConstants.CpsConstants.BLOCK_PLACE_CPS
     local old = getmetatable(bedwars["SwordController"]).isClickingTooFast
@@ -1839,6 +1889,35 @@ end
 
 -- // movement window 
 
+do 
+    local Anti = {["Enabled"] = false}
+    Anti = GuiLibrary["Objects"]["MovementWindow"]["API"].CreateOptionsButton({
+        ["Name"] = "Anticheat bypass(shit version)",
+        ["Function"] = function(callback) 
+            if callback then 
+                local lplr = game.Players.LocalPlayer
+        getgenv().cower = true
+
+        if getgenv().cower then
+          spawn(function()
+            repeat
+              if (not getgenv().cower) then
+                return end
+                wait()
+        
+                local AnimC = Instance.new("Animation")
+                AnimC.AnimationId = "http://www.roblox.com/asset/?id=4940563117"
+                
+                lplr.Character.Animate.Disabled = true
+                local toplay = lplr.Character.Humanoid.Animator:LoadAnimation(AnimC)
+                toplay:Play(0.1,1,1)
+              until (not getgenv().cower)
+            end)
+          end
+            end
+        end
+    })
+end
 
 -- ac assist
 
